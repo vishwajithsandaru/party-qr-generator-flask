92a94b40af6d2f448888ee012ed086d337c6f4a2
vishwajithsandaru <sandarukasthoori@gmail.com>
2022-03-29 11:32:46 +0530

    Changes added

--- src/server.py
+++ src/server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
+import base64
+import os
+from Crypto.Cipher import AES
 
 app = Flask(__name__)
+
+secret = 'TESTTESTTESTTEST'
 
 @app.route("/ping")
 def ping():
@@ -8,3 +13,14 @@ def ping():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+def get_decoded_secret():
+    decoded_string = base64.b64decode(secret)
+    return decoded_string
+
+def decrypt_message(msg, key):
+	decoded_encrypted_msg = base64.b64decode(msg)
+	cipher = AES.new(key)
+	decrypted_msg = cipher.decrypt(decoded_encrypted_msg)
+	unpadded_private_msg = decrypted_msg.rstrip(padding_character)
+	return unpadded_private_msg
