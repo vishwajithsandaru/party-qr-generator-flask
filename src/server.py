9f0e820cdfeb2a6b35ca2c587e3661b0337ec3da
vishwajithsandaru <sandarukasthoori@gmail.com>
2022-03-29 21:50:22 +0530

    Changes added.

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, request, jsonify
 import base64
 import os
@@ -7,20 +8,24 @@ app = Flask(__name__)
 
 secret = 'TESTTESTTESTTEST'
 
-@app.route("/ping")
-def ping():
-    return jsonify({"status": "success"})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
-
-def get_decoded_secret():
-    decoded_string = base64.b64decode(secret)
-    return decoded_string
+def unpad_str(msg):
+    msg = msg.decode('utf-8')
+    msg = msg.rstrip('%')
+    return msg
 
 def decrypt_message(msg, key):
 	decoded_encrypted_msg = base64.b64decode(msg)
 	cipher = AES.new(key)
 	decrypted_msg = cipher.decrypt(decoded_encrypted_msg)
-	unpadded_private_msg = decrypted_msg.rstrip(padding_character)
+	unpadded_private_msg = unpad_str(decrypted_msg)
 	return unpadded_private_msg
+
+@app.route("/decrypt", methods=['GET'])
+def decrypt():
+    args = request.args
+    encrypted_string = args.get('enc_str')
+    dec_str = decrypt_message(encrypted_string, secret) 
+    return jsonify({"status": str(dec_str)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
